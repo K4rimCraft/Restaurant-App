8728340a1b6492f10087e59fcd7294be04fc08df
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2023-11-25 01:18:40 +0200

    :):)

--- RESTO/services/authService.js
+++ RESTO/services/authService.js
@@ -5,21 +5,21 @@ const ApiError = require("../utils/apiError");
 
 
 const register = asyncHandelr(async (req, res, next) => {
-    const [rows1] = await (await dbConnection).query('SELECT * FROM persons where email = ?', [req.body.personId]);
+    const [rows1] = await (await dbConnection).query('SELECT * FROM persons where personId = ?', [req.body.personId]);
     if (rows1.length !== 0) {
         return next(new ApiError(`Wrong Email Or Password`, 404));
     }
     const {
-        personId,firstName,lastName,
-        email,password,birthDate,
-        longitudeAddress,latitudeAddress,
-        phoneNumber,type } = req.body;
+        personId, firstName, lastName,
+        email, password, birthDate,
+        longitudeAddress, latitudeAddress,
+        phoneNumber, type } = req.body;
     const [rows2] = await (await dbConnection).query(`INSERT INTO persons VALUES(?,?,?,?,?,?,?,?,?,?)`,
         [personId, firstName, lastName, email, password, birthDate, longitudeAddress, latitudeAddress, phoneNumber, type]);
-        const token = jwt.sign({ id: personId, type: type }, process.env.JWT_SECRETKEY, {
-            expiresIn: "90d"
-        });
-    res.json({token});
+    const token = jwt.sign({ id: personId, type: type }, process.env.JWT_SECRETKEY, {
+        expiresIn: "90d"
+    });
+    res.json({ token });
 }
 );
 
